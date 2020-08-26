969c0397a7370b1c4b8e7fc36820b3d8e2a02b2c
Chris Stivers <cstivers@atlassian.com>
2020-08-26 10:25:25 -0700

    added rego support

---
+++ components/prism-rego.js
@@ -0,0 +1,26 @@
+(function (Prism) {
+
+  Prism.languages.rego = Prism.languages.extend('clike', {
+    
+    'comment': {
+      pattern: /(^|[^\\])#.*/,
+      lookbehind: true
+    },
+    
+    'string': {
+      pattern: /(["'])(?:\1\1|\\(?:\r\n|[\s\S])|(?!\1)[^\\\r\n])*\1/,
+      greedy: true
+    },
+    
+    'class-name': /(?:@\{[\w-]+\}|[^{};\s@])(?:@\{[\w-]+\}|\((?:[^(){}]|\([^(){}]*\))*\)|[^(){};@])*?(?=\s*\{)/,
+    
+    'function': /\w+(?=\()/,
+
+    'number': /\b\d+\.?\d*/,
+        
+    'punctuation': /[(){}\[\],]/,
+    
+    'keyword': /\b(?:as|default|else|false|import|package|not|null|true|with)\b/,
+  });
+
+}(Prism));
